02f2cf73643fd145d46844b4b3fa9c0dded420bf
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-04 11:06:44 +0630

    Add tests for ship object

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -1,3 +1,32 @@
 import Ship from '../src/factoryFunctions/ship';
 
-describe('Ship', () => {});
+describe('Ship', () => {
+  describe('hit', () => {
+    it('records a hit', () => {
+      const ship = Ship('Destroyer', 3);
+      ship.hit([1, 2]);
+      const hitPositions = ship.getHitPositions();
+      expect(hitPositions).toEqual([[1, 2]]);
+    });
+  });
+
+  describe('isSunk', () => {
+    it('returns true if the length is equal to the length of the hit array', () => {
+      const ship = Ship('Patrol Boat', 2);
+      ship.hit([0, 0]);
+      ship.hit([0, 1]);
+      const isSunk = ship.isSunk();
+
+      expect(isSunk).toBeTruthy();
+    });
+
+    it('returns false if the length is not equal to the length of the hit array', () => {
+      const ship = Ship('Submarine', 3);
+      ship.hit([0, 0]);
+      ship.hit([0, 1]);
+      const isSunk = ship.isSunk();
+
+      expect(isSunk).not.toBeTruthy();
+    });
+  });
+});
