f0d98c766fcc5a76dc7c004437b4d0d1664367fc
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-04 11:06:24 +0630

    Add factory function for ship object

--- src/factoryFunctions/ship.js
+++ src/factoryFunctions/ship.js
@@ -1,3 +1,20 @@
-const Ship = () => {};
+const Ship = (type, length) => {
+  const hitPositions = [];
+
+  const getHitPositions = () => hitPositions;
+
+  const getType = () => type;
+
+  const hit = (coordinate) => hitPositions.push(coordinate);
+
+  const isSunk = () => length === hitPositions.length;
+
+  return {
+    getHitPositions,
+    getType,
+    hit,
+    isSunk,
+  };
+};
 
 export default Ship;
