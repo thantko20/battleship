368e4f43e8e0bfc03a47a9c0271e7f8e646d2bd6
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-04 12:11:38 +0630

    Add functions for placing patrol boat

--- src/factoryFunctions/gameBoard.js
+++ src/factoryFunctions/gameBoard.js
@@ -1,3 +1,52 @@
-const Gameboard = () => {};
+import Ship from './ship';
+
+const Gameboard = () => {
+  const ships = [];
+
+  // Returns an array of positions based on initialCoor and shipLength and X axis
+  const positionsXaxis = (shipLength, initialCoordinate) => {
+    const positions = [initialCoordinate];
+
+    for (let i = 0; i < shipLength; i += 1) {
+      positions.push([initialCoordinate[0], initialCoordinate[1] + 1]);
+    }
+
+    return positions;
+  };
+
+  // Returns an array of positions based on initialCoor and shipLength and Y axis
+  const positionsYaxis = (shipLength, initialCoordinate) => {
+    const positions = [initialCoordinate];
+
+    for (let i = 0; i < shipLength; i += 1) {
+      positions.push([initialCoordinate[0] + 10, initialCoordinate[1]]);
+    }
+
+    return positions;
+  };
+
+  const positionsOnGivenAxis = (shipLength, initialCoordinate, axis) => {
+    if (axis === 'X') {
+      return positionsXaxis(shipLength, initialCoordinate);
+    }
+
+    return positionsYaxis(shipLength, initialCoordinate);
+  };
+
+  const placeShip = (type, coordinate, axis) => {
+    if (type === 'Patrol Boat') {
+      const positions = positionsOnGivenAxis(1, coordinate, axis);
+
+      ships.push({ ship: Ship(type, 2), positions });
+    }
+  };
+
+  const getShips = () => ships;
+
+  return {
+    placeShip,
+    getShips,
+  };
+};
 
 export default Gameboard;
