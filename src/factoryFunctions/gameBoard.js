4da44c52678befd3aab6f7699760bba61af845f8
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-04 17:21:38 +0630

    Remove coordinates evaluation from placeShip. It will only place the ships at given coordinates parameter

--- src/factoryFunctions/gameBoard.js
+++ src/factoryFunctions/gameBoard.js
@@ -3,45 +3,9 @@ import Ship from './ship';
 const Gameboard = () => {
   const ships = [];
 
-  // Returns an array of positions based on initialCoor and shipLength and X axis
-  const positionsXaxis = (shipLength, initialCoordinate) => {
-    const positions = [];
-
-    for (let i = 0; i < shipLength; i += 1) {
-      positions.push([initialCoordinate[0], initialCoordinate[1] + 1 * i]);
-    }
-
-    return positions;
-  };
-
-  // Returns an array of positions based on initialCoor and shipLength and Y axis
-  const positionsYaxis = (shipLength, initialCoordinate) => {
-    const positions = [];
-
-    for (let i = 0; i < shipLength; i += 1) {
-      positions.push([initialCoordinate[0] + 10 * i, initialCoordinate[1]]);
-    }
-
-    return positions;
-  };
-
-  const placeOnGivenAxis = (shipLength, initialCoordinate, axis, type) => {
-    let positions = [];
-    if (axis === 'X') {
-      positions = positionsXaxis(shipLength, initialCoordinate);
-    } else {
-      positions = positionsYaxis(shipLength, initialCoordinate);
-    }
-
-    ships.push({ ship: Ship(type, shipLength), positions });
-  };
-
-  const placeShip = (type, coordinate, axis) => {
-    if (type === 'Patrol Boat') {
-      placeOnGivenAxis(2, coordinate, axis, type);
-    } else if (type === 'Submarine') {
-      placeOnGivenAxis(3, coordinate, axis, type);
-    }
+  const placeShip = (type, coordinates) => {
+    const ship = Ship(type, coordinates.length);
+    ships.push({ ship, coordinates });
   };
 
   const getShips = () => ships;
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -2,15 +2,31 @@ import Gameboard from '../src/factoryFunctions/gameBoard';
 
 describe('Gameboard', () => {
   describe('placeShip', () => {
-    it('places a patrol boat vertically', () => {
+    it('places the patrol boat at given coordinates', () => {
       const board = Gameboard();
-      board.placeShip('Patrol Boat', [0, 0], 'Y');
-      const occupiedCoordinates = board.getShips()[0].positions;
-      const expected = [
+      const coordinates = [
         [0, 0],
-        [10, 0],
+        [0, 1],
       ];
-      expect(occupiedCoordinates).toEqual(expected);
+      board.placeShip('Patrol Boat', coordinates);
+      const placedCoordinates = board.getShips()[0].coordinates;
+
+      expect(placedCoordinates).toBe(coordinates);
+    });
+
+    it('places the carrier at given coordinates', () => {
+      const board = Gameboard();
+      const coordinates = [
+        [2, 0],
+        [3, 0],
+        [4, 0],
+        [5, 0],
+        [6, 0],
+      ];
+      board.placeShip('Carrier', coordinates);
+      const placedCoordinates = board.getShips()[0].coordinates;
+
+      expect(placedCoordinates).toBe(coordinates);
     });
   });
 });
