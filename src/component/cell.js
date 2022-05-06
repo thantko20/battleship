8b51ec0dece1e992e53c5e4377aacb156d1156db
Thant Ko Zaw <thantmkou10@gmail.com>
2022-05-06 18:07:27 +0630

    Add cell component

---
+++ src/component/cell.js
@@ -0,0 +1,10 @@
+const cell = (playerType, coorindate, empty = true) => {
+  const container = document.createElement('div');
+  container.className = `grid-cell ${playerType}`;
+  container.setAttribute('empty', empty);
+  container.setAttribute('coordinate', `${coorindate[0]},${coorindate[1]}`);
+
+  return container;
+};
+
+export default cell;
